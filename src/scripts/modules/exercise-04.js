1557bbe4427e20302ec24a13ed4f21c8c61e4e42
christianwl <chris2w0social@gmail.com>
2026-02-24 20:35:38 -0300

    refactor: add exercice-04.js

---
+++ src/scripts/modules/exercise-04.js
@@ -0,0 +1,20 @@
+// Atividade 4
+/*
+Faça um programa que leia 3 valores informados pelo usuário 
+(considere que não serão informados valores iguais) e escrever a soma dos 2 maiores.
+*/
+
+export function showExercice04() {
+  const qnt = 3;
+  let numberList = getDistinctUserNumbers(qnt);
+
+  let twoMaxNumbers = getListTwoMaxNumbers(numberList);
+  let sumMaxNumbers = twoMaxNumbers.reduce((result, value) => result + value, 0);
+
+  alert(`A soma dos dois maiores números, ${twoMaxNumbers[0]} e ${twoMaxNumbers[1]} é: ${sumMaxNumbers}`,);
+}
+
+function getListTwoMaxNumbers(array = []) {
+  let descSortedList = [...array].sort((a, b) => b - a);
+  return descSortedList.slice(0, 2);
+}
